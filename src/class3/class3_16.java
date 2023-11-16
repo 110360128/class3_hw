c583f7e09f90d020f1c85e264793f93441c0b0af
110360128 <a40997596@gmail.com>
2023-11-16 23:14:38 +0800

    class3_hw

---
+++ src/class3/class3_16.java
@@ -0,0 +1,22 @@
+package class3;
+
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.IOException;
+
+public class class3_16 {
+	public static void main(String[] args)throws IOException
+	{
+		System.out.println("請輸入1個整數");
+		
+		BufferedReader br= 
+				new BufferedReader(new InputStreamReader (System.in));
+		
+		String str=br.readLine();
+		
+		int num = Integer.parseInt(str);
+		
+		System.out.println("你輸入的數字是"+num);
+	}
+	
+}
